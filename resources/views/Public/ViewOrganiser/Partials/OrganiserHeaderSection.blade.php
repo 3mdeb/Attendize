85d37af20a62677d46639dd5fcab2fcfd9d3e616
Norbert Kamiński <norbert.kaminski@3mdeb.com>
2023-02-24 14:11:04 +0100

    views: Remove unneeded sections and adjust organizer header
    Signed-off-by: Norbert Kamiński <norbert.kaminski@3mdeb.com>

--- resources/views/Public/ViewOrganiser/Partials/OrganiserHeaderSection.blade.php
+++ resources/views/Public/ViewOrganiser/Partials/OrganiserHeaderSection.blade.php
@@ -1,12 +1,11 @@
 <section id="intro" class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="organiser_logo">
-                <div class="thumbnail">
-                    <img src="{{URL::to($organiser->full_logo_path)}}" />
+            <div>
+                <div>
+                    <img class="center" src="{{URL::to($organiser->full_logo_path)}}" />
                 </div>
             </div>
-            <h1>{{$organiser->name}}</h1>
             @if($organiser->about)
             <div class="description pa10">
                 {!! md_to_html($organiser->about) !!}
